42643cb94f3d33b54f09ee3dac6bd6e0f1d04390
Zakhar <qwe@MacBook-Air-Zakhar.local>
2022-12-16 12:58:52 +0300

    lab6

--- test.py
+++ test.py
@@ -1,18 +1,14 @@
-import unittest
+import pytest
 from postfix_calculator import PostfixCalculator
 
 
-class PostfixCalculatorTest(unittest.TestCase):
-
-    def test1(self):
-        self.assertEqual(PostfixCalculator.compute("1 4 + 48 12 - *"), 180)
-
-    def test2(self):
-        self.assertEqual(PostfixCalculator.compute("74 12 + 53 12 - * 151 /"), 23)
-
-    def test3(self):
-        self.assertEqual(PostfixCalculator.compute("123 12 / 123 * 4221 -"), -2991)
+def test_1():
+    assert PostfixCalculator.compute("1 4 + 48 12 - *") == 180
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_2():
+    assert PostfixCalculator.compute("74 12 + 53 12 - * 151 /") == 23
+
+
+def test_3():
+    assert PostfixCalculator.compute("123 12 / 123 * 4221 -") == -2991
